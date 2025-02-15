11725aac273aa3f6c266ce949050b77f32156f4b
Hardik-Kumar1912 <hardikk1401@gmail.com>
2025-02-15 12:22:29 +0530

    2nd part completed

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div>
+      <div className='px-4 flex-1 overflow-auto py-4'>
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+ 			<Message />
+			<Message />
+ 			<Message />
+ 			<Message />
+ 		</div>
+    </div>
+  )
+}
+
+export default Messages
